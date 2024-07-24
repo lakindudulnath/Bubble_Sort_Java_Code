6044467f45e989485ff428526c7b4a53846c8a78
lakindudulnath <lakindu.dulnath.123@gmail.com>
2024-07-24 05:50:50 +0530

    Add files via upload

---
+++ BubbleSortOptimized.java
@@ -0,0 +1,33 @@
+package Bubble_sort_optimized_code_java;
+public class BubbleSortOptimized {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(array);
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break; // No swaps means the array is already sorted
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
